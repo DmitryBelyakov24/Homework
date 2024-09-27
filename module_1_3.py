59e764440fe815bb8a7ab3132d8f35d4d9dbe62e
DmitryBelyakov24 <d.belyackov@gmail.com>
2024-09-27 11:40:59 +0300

    Homeworks_2
    Практическая работа по уроку "Динамическая типизация

---
+++ module_1_3.py
@@ -0,0 +1,22 @@
+# Практическая работа по уроку "Динамическая типизация"
+# Цель: Написать программу на языке Python, используя Pycharm, для демонстрации динамической типизации.
+
+name = "Dmitry"
+
+print("Name:", name) # Вывод перменной 'name' на экран
+# print(type(name)) # Вывод на экран значенния типа переменной
+# print(name, type(name)) # Вывод на экран переменной и значение типа переменной
+
+age = 40
+
+print("Age:", age)
+
+# a = 15
+# print (age + a)
+
+NewAge = age + 1
+print("New Age:", NewAge)
+
+is_student = "Dmitry"
+
+print("Is Student:", is_student == name )
