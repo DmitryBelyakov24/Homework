ae808378ee6e2f6ef609b66db56e90065eca9a5e
DmitryBelyakov24 <d.belyackov@gmail.com>
2024-09-28 14:43:40 +0300

    Homeworks_5
    Практическое задание по уроку "Строки и индексация строк

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+# Практическая работа по уроку "Организация программ и методы строк".
+print('Практическая работа по уроку "Организация программ и методы строк."')
+
+my_string = input("Введите название Университета и год через пробел  :")
+
+print(my_string .upper())
+print(my_string.lower())
+print(my_string .replace(' ', ''))
+print(my_string[0])
+print(my_string[9])
+
